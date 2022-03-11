4511aba933812384f2dc2c6bfa6e5d0fe3e723fa
StonksSelected <62770315+StonksSelected@users.noreply.github.com>
2022-03-11 19:22:59 +0000

    Update songguesser.py

--- songguesser.py
+++ songguesser.py
@@ -1,32 +1,72 @@
-import simple_chalk
 import random
 
 usernames = ["test", "test1"]
 passwords = ["test", "test1"]
 guesses = 0
-
+printsong = ""
+count = 0
+wincount = 0
+gamecount = 1
+global song
 with open('songs.txt') as songs:
-    x = len(songs.readlines())
+    songs.seek(0)
+    x = len(songs.readlines()) - 1
     linenum = random.randint(0, x)
     songs.seek(0)
-    song = songs.readline(linenum)
+    song = songs.readlines(linenum)[0]
 with open('artists.txt') as artists:
     artists.seek(0)
-    artist = artists.readline(linenum)
+    artist = artists.readlines(linenum)[0]
 
-print(song)
-
-songprint = song.split()
-for x in range(0, len(songprint)):
-    printsong = songprint[x]
+songsplit = song.split()
+while count != len(songsplit):
+    holder = songsplit[count]
+    if count == 0:
+        printsong += holder[0]
+        for z in range(0, len(holder) - 1):
+            printsong += "-"
+        count += 1
+    else:
+        printsong += " " + holder[0]
+        for z in range(0, len(holder) - 1):
+            printsong += "-"
+        count += 1
 
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 if username in usernames and password in passwords and passwords.index(password) == usernames.index(username):
-    print("test")
-    while guesses <= 0 and guesses >= 1:
-        print(artist)
-        print()
+    while guesses >= 0 and guesses <= 1:
+        print("The artist is", artist)
+        print("Song hint", printsong)
         guess = input("Guess song name: ")
+        if guess == song:
+            guesses = 0
+            gamecount += 1
+            wincount += 1
+            print("You have won", wincount, "game(s)")
+            x = len(songs.readlines()) - 1
+            linenum = random.randint(0, x)
+            songs.seek(0)
+            song = songs.readlines(linenum)
+            song = song[0] #Casts song from array into a str
+            artists.seek(0)
+            artist = artists.readlines(linenum)
+            artist = artist[0] #Casts song from array into a str
+            songsplit = song.split()
+            while count != len(songsplit):
+                holder = songsplit[count]
+                if count == 0:
+                    printsong += holder[0]
+                    for z in range(0, len(holder) - 1):
+                        printsong += "-"
+                    count += 1
+                else:
+                    printsong += " " + holder[0]
+                    for z in range(0, len(holder) - 1):
+                        printsong += "-"
+                    count += 1
+        else:
+            guesses += 1
+    print("You lost after", gamecount, "game(s)")
 else:
     exit()
