9bc0ac13134ce681010292fd8a1572211913e418
Meh1239 <17digbyb@highfield.herts.sch.uk>
2022-03-11 17:19:57 +0000

    push

--- songguesser.py
+++ songguesser.py
@@ -4,30 +4,27 @@ import random
 usernames = ["test", "test1"]
 passwords = ["test", "test1"]
 guesses = 0
-with open('songs.txt', 'r') as songs:
-    count = 0
-    for line in fp:
-        if line != "\n":
-            count += 1
-    linenum = random.randint(0, count - 1)
-    song = songs.readline(linenum)
 
-with open('artists.txt', 'r') as artists:
+with open('songs.txt') as songs:
+    x = len(songs.readlines())
+    linenum = random.randint(0, x)
+    song = songs.readline(linenum)
+with open('artists.txt') as artists:
     artist = artists.readline(linenum)
 
-print(linenum)
 print(song)
+
 songprint = song.split()
-for y in range(0, len(songprint)):
-    printsong = songprint[y]
+for x in range(0, len(songprint)):
+    printsong = songprint[x]
+
 username = input("Enter your username: ")
 password = input("Enter your password: ")
-
-for z in range(0,len(usernames)):
-    if username == usernames[z] and password == passwords[z]:
-        while guesses <= 0 and guesses >= 1:
-            print(artist)
-            print()
-            guess = input("Guess song name: ")
-    else:
-        exit()
+if username in usernames and password in passwords and passwords.index(password) == usernames.index(username):
+    print("test")
+    while guesses <= 0 and guesses >= 1:
+        print(artist)
+        print()
+        guess = input("Guess song name: ")
+else:
+    exit()
