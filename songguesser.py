75765228d8ea864652eecd0ade71e621e2ee8933
Meh1239 <97896007+Meh1239@users.noreply.github.com>
2022-03-11 20:08:48 +0000

    Update songguesser.py

--- songguesser.py
+++ songguesser.py
@@ -1,70 +1,50 @@
 import random
-
 usernames = ["test", "test1"]
 passwords = ["test", "test1"]
 guesses = 0
-printsong = ""
-count = 0
 wincount = 0
-gamecount = 1
-global song
-with open('songs.txt') as songs:
-    songs.seek(0)
-    x = len(songs.readlines()) - 1
-    linenum = random.randint(0, x)
-    songs.seek(0)
-    song = songs.readlines(linenum)[0]
-with open('artists.txt') as artists:
-    artists.seek(0)
-    artist = artists.readlines(linenum)[0]
+gamecount = 1   
 
-songsplit = song.split()
-while count != len(songsplit):
-    holder = songsplit[count]
-    if count == 0:
-        printsong += holder[0]
-        for z in range(0, len(holder) - 1):
-            printsong += "-"
-        count += 1
-    else:
-        printsong += " " + holder[0]
-        for z in range(0, len(holder) - 1):
-            printsong += "-"
-        count += 1
+def gensong():
+    linenum = random.randint(0, len(open("songs.txt").readlines()) - 1)
+    global song
+    global artist
+    song = open("songs.txt").readlines(linenum)[0]
+    artist = open("artists.txt").readlines(linenum)[0]
+
+def hintify(word):
+    songhint = ""
+    count = 0
+    songsplit = word.split()
+    while count != len(songsplit):
+        holder = songsplit[count]
+        if count == 0:
+            songhint += holder[0]
+            for i in range(0, len(holder) - 1):
+                songhint += "-"
+            count += 1
+        else:
+            songhint += " " + holder[0]
+            for i in range(0, len(holder) - 1):
+                songhint += "-"
+            count += 1
+    return songhint
 
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 if username in usernames and password in passwords and passwords.index(password) == usernames.index(username):
+    gensong()
     while guesses >= 0 and guesses <= 1:
         print("The artist is", artist)
-        print("Song hint", printsong)
+        print("Song hint", hintify(song))
         guess = input("Guess song name: ")
         if guess == song:
             guesses = 0
             gamecount += 1
             wincount += 1
             print("You have won", wincount, "game(s)")
-            x = len(songs.readlines()) - 1
-            linenum = random.randint(0, x)
-            songs.seek(0)
-            song = songs.readlines(linenum)
-            song = song[0] #Casts song from array into a str
-            artists.seek(0)
-            artist = artists.readlines(linenum)
-            artist = artist[0] #Casts song from array into a str
-            songsplit = song.split()
-            while count != len(songsplit):
-                holder = songsplit[count]
-                if count == 0:
-                    printsong += holder[0]
-                    for z in range(0, len(holder) - 1):
-                        printsong += "-"
-                    count += 1
-                else:
-                    printsong += " " + holder[0]
-                    for z in range(0, len(holder) - 1):
-                        printsong += "-"
-                    count += 1
+            gensong()
+            hintify(song)
         else:
             guesses += 1
     print("You lost after", gamecount, "game(s)")
